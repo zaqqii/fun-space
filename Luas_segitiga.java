1df94bca4653e7887759a9c2b21d6038bdd85ff4
microsoft <zakirahman566@gmail.com>
2024-09-06 10:44:58 +0700

    First GIthub project

---
+++ Luas_segitiga.java
@@ -0,0 +1,16 @@
+public class luas_segitiga{
+    public static void main (String[]args) {
+        // deklarasi variabel
+        double alas, tinggi, luas;
+
+        // installasasi variabel
+        alas=5;
+        tinggi=10;
+        luas=0.5*alas*tinggi;
+    
+        //cetak hasil
+        System.out.println("luas segitiga adalah"+luas);
+
+    }
+
+}
